471f46ecd67e5f78f457c3f4bd7028949cf41274
Austin Dungan <36718949+ajdungan@users.noreply.github.com>
2021-12-17 20:39:19 -0700

    Update glancesinstall.sh

--- glancesinstall.sh
+++ glancesinstall.sh
@@ -4,7 +4,7 @@
 #####install glances via pip
 apt update
 apt install python-pip -y
-pip install 'lances[action,browser,cloud,cpuinfo,docker,export,folders,gpu,graph,ip,raid,snmp,web,wifi]'
+pip install 'glances[action,browser,cloud,cpuinfo,docker,export,folders,gpu,graph,ip,raid,snmp,web,wifi]'
 
 #alternative install script
 #curl -L https://bit.ly/glances | /bin/bash
