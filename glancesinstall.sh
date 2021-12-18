141d42aa6a872f458532f7f205c7f35af3f0f144
Austin Dungan <36718949+ajdungan@users.noreply.github.com>
2021-12-17 20:32:37 -0700

    Create glancesinstall.sh

---
+++ glancesinstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#must run as root!
+
+#####install glances via pip
+apt update
+apt install python-pip -y
+pip install 'lances[action,browser,cloud,cpuinfo,docker,export,folders,gpu,graph,ip,raid,snmp,web,wifi]'
+
+#alternative install script
+#curl -L https://bit.ly/glances | /bin/bash
+
+
+##########
+#run setup scripts to create persistance startup service
+
+./glancessetup.sh
+./glancesservice.sh
+
