7e719848ebf8f8e9f21d5ba1211c2735366b5338
Austin Dungan <36718949+ajdungan@users.noreply.github.com>
2021-12-20 19:56:46 -0700

    Create nvidia-drivers.sh

---
+++ nvidia-drivers.sh
@@ -0,0 +1,6 @@
+#add ppa
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+
+#install driver, confirm appropriate/best driver for your card,as of now for rtx 3090 it is the 495 driver
+sudo apt install nvidia-graphics-drivers-495 -y
