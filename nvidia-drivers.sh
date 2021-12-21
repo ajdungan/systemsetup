ce439fb764d9cebf22fa035357286ef00a6f690b
Austin Dungan <36718949+ajdungan@users.noreply.github.com>
2021-12-20 20:02:43 -0700

    Update nvidia-drivers.sh

--- nvidia-drivers.sh
+++ nvidia-drivers.sh
@@ -3,4 +3,4 @@ sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt-get update
 
 #install driver, confirm appropriate/best driver for your card,as of now for rtx 3090 it is the 495 driver
-sudo apt install nvidia-graphics-drivers-495 -y
+sudo apt install nvidia-driver-495 -y
