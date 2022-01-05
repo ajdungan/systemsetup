380060fb825e4536229aaa8f24b1624ac6550e37
Austin Dungan <36718949+ajdungan@users.noreply.github.com>
2022-01-05 00:07:00 +0000

    Update gnomeinstall.sh

--- gnomeinstall.sh
+++ gnomeinstall.sh
@@ -9,8 +9,26 @@ sudo apt install gnome-themes-ubuntu
 sudo apt install gnome-tweaks -y
 
 
+sudo apt install gnome-shell-extensions-gpaste gpaste
+
+
 #apps
-sudo apt install gparted arandr synaptic -y
+sudo apt install gparted arandr synaptic htop -y
 
 #apps to remove
 sudo apt purge snapd -y
+
+
+#ppa apps
+#stacer
+sudo add-apt-repository ppa:oguzhaninan/stacer
+sudo apt-get update
+sudo apt install stacer -y
+
+#compile/install from source apps
+ apt install cmake libncurses5-dev libncursesw5-dev git
+ git clone https://github.com/Syllo/nvtop.git
+ mkdir -p nvtop/build && cd nvtop/build
+ cmake ..
+ make
+ make install
